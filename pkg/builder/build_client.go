3e7a03a9877660b95aff5941820eb7beae5a4496
sar <Steve.Rago@ny.email.gs.com>
2024-12-31 14:21:56 -0500

    add some logging to the worker

--- pkg/builder/build_client.go
+++ pkg/builder/build_client.go
@@ -263,8 +263,10 @@ func (bc *BuildClient) Run(ctx context.Context) (bool, error) {
 		bc.touchSchedulerMayThinkExecuting()
 	}
 	if err != nil {
+		log.Printf("Failed to synchronize with scheduler: %v", err)
 		return false, util.StatusWrap(err, "Failed to synchronize with scheduler")
 	}
+	log.Printf("Synchronized with scheduler")
 
 	// Determine when we should contact the scheduler again in case
 	// of no activity.
